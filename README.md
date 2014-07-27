ecc04605f44a73c910a314652c1a66f734326cc8
Mark Innes <innesm4@gmail.com>
2014-07-27 16:32:52 +0100

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-dinosaur_konami.js
-==================
+dinosaur_konami.js # [![Build Status](https://travis-ci.org/yeoman/yo.svg?branch=master)](https://travis-ci.org/yeoman/yo) ![](http://img.shields.io/badge/unicorn-approved-ff69b4.svg)
+
+"Finally a Konami Code Plugin I can use"
+
+## Installation
+
+Requires jQuery 
+
+```
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="dinosaur_konami.js"></script> 
+```
+
+Options
+```
+var species = $.extend({
+                dino: "img/dino-head.png",
+                height: "250",
+                width: "250",
+                top: "50%",
+                left: "50%",
+            }, trex),
+```
+
+Add to Page
+```
+var Dinosaur = {
+	    init: function() {
+
+	        $('body').dinosaur_konami();
+	    
+	    }
+	};
+Dinosaur.init();	
+```
+
+Enter the Konami Code
+```
+UP UP DOWN DOWN LEFT RIGHT LEFT RIGHT B A
+```
+
+Move Dino
+```
+UP DOWN LEFT RIGHT
+```
+
+## Changelog
+
+1.0 - Initial Release
+
+## License
+
+[BSD license](http://opensource.org/licenses/bsd-license.php)
---
+++ dinosaur_konami.js
@@ -0,0 +1,95 @@
+(function($) {
+
+    $.fn.dinosaur_konami = function(trex) {
+
+        if (window.addEventListener) {
+
+            var species = $.extend({
+                dino: "img/dino-head.png",
+                height: "250",
+                width: "250",
+                top: "50%",
+                left: "50%",
+            }, trex),
+                keys = [],
+                konami_dino = "38,38,40,40,37,39,37,39,66,65";
+
+            window.addEventListener("keydown", function(e) {
+                keys.push(e.keyCode);
+
+                if (keys.toString().indexOf(konami_dino) >= 0) {
+
+                    add_dinosaur();
+                    style_dino();
+
+                    $('.dinosaur_konami_trex').fadeIn();
+
+                    keys = [];
+                }
+            }, true);
+        }
+
+        function style_dino() {
+            $('.dinosaur_konami_trex').css({
+                'position': 'absolute',
+                'left': species.left,
+                'top': species.top,
+                'display': 'none',
+                'height': species.height + 'px',
+                'width': species.width + 'px',
+                'margin-top': '-' + species.height / 2 + 'px',
+                'margin-left': '-' + species.width / 2 + 'px',
+                'z-index': '50',
+            });
+        }
+
+        function add_dinosaur() {
+            var t_rex = $('<div class="dinosaur_konami_trex"><img src="' + species.dino + '" /></div>');
+            $(t_rex).appendTo('body');
+            move_dinosaur();
+
+            $('.dinosaur_konami_trex').click(function() {
+                $(this).fadeOut().remove();
+            });
+        }
+
+        function move_dinosaur() {
+
+            window.addEventListener("keydown", function(e) {
+                switch (e.which) {
+                    case 37: //left
+                        $('.dinosaur_konami_trex').stop().animate({
+                            left: '-=100',
+                            duration: 'slow',
+                            easing: 'swing',
+                        });
+                        break;
+                    case 38: //up
+                        $('.dinosaur_konami_trex').stop().animate({
+                            top: '-=100',
+                            duration: 'slow',
+                            easing: 'swing',
+                        });
+                        break;
+                    case 39: //right
+                        $('.dinosaur_konami_trex').stop().animate({
+                            left: '+=100',
+                            duration: 'slow',
+                            easing: 'swing',
+                        });
+                        break;
+                    case 40: //bottom
+                        $('.dinosaur_konami_trex').stop().animate({
+                            top: '+=100',
+                            duration: 'slow',
+                            easing: 'swing',
+                        });
+                        break;
+                }
+            });
+        }
+
+        return this;
+    };
+
+})(jQuery);
